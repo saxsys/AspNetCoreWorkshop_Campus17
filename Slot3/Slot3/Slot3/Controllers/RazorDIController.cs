801289be1c742211b7ced8e058f969d88d272c15
Marcus Kohnert <marcus.dev@live.com>
2017-06-16 14:06:45 +0200

    Added razor DI sample

---
+++ Slot3/Slot3/Slot3/Controllers/RazorDIController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Slot3.Controllers
+{
+    public class RazorDIController : Controller
+    {
+        public IActionResult Index() => this.View();
+    }
+}

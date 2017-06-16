ddc481c04e8e2a40c2152755dbc617b4be3a5a81
Marcus Kohnert <marcus.dev@live.com>
2017-06-16 14:23:06 +0200

    Added IHtmlHelper Sample

---
+++ Slot3/Slot3/Slot3/Controllers/CustomHtmlHelperController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Slot3.Controllers
+{
+    public class CustomHtmlHelperController : Controller
+    {
+        public IActionResult Index() => this.View();
+    }
+}

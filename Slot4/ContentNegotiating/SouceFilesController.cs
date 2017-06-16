e441e99995fce454ec75c621a73fd02b79ca7751
Marcus Kohnert <marcus.dev@live.com>
2017-06-16 10:53:54 +0200

    Added modelbinding negative example

---
+++ Slot4/ContentNegotiating/SouceFilesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContentNegotiating
+{
+    [Route("sourcefiles")]
+    public class SouceFilesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(string name, string author)
+        {
+            var sourceFile = new SourceFile { Name = name, Author = new Author { Name = author } };
+
+            // TODO: Persist
+
+            return this.Created("/SourceFiles/1", sourceFile.Name);
+        }
+    }
+}

67d4ce3c1ce24abbb33a089ba213c8f1e1c57f42
Susann Schmidt <susann.schmidt@saxsys.de>
2017-06-21 18:24:02 +0200

    add attribut to taghelper

--- Slot3/Slot3/Slot3/TagHelpers/EmailTagHelper.cs
+++ Slot3/Slot3/Slot3/TagHelpers/EmailTagHelper.cs
@@ -1,14 +1,26 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Slot3.Controllers;
 using System.Threading.Tasks;
 
 namespace Slot3.TagHelpers
 {
-    [HtmlTargetElement("email")]
+    //[HtmlTargetElement("email")]
     public class EmailTagHelper : TagHelper
     {
+        private const string EmailDomain = "saxsys.de";
+        
+        // Can be passed via <email mail-to="..." />. 
+        // Pascal case gets translated into lower-kebab-case.
+        public string MailTo { get; set; }
+
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";    // Replaces <email> with <a> tag
+
+            var address = MailTo + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + address);
+            
         }
     }
 }

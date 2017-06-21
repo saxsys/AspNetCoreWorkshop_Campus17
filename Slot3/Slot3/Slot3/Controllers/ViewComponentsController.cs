7801148b1121efe2c9c525edbbd8175133da72d6
Susann Schmidt <susann.schmidt@saxsys.de>
2017-06-21 19:08:04 +0200

    add ViewComponents

---
+++ Slot3/Slot3/Slot3/Controllers/ViewComponentsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Slot3.Controllers
+{
+    public class ViewComponentsController:Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Slot3/Slot3/Slot3/ViewComponents/DateTimeNowViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Slot3.ViewComponents
+{
+    //[ViewComponent(Name = "Now")]
+    public class DateTimeNowViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(bool tomorrow)
+        {
+
+            var timeNow = DateTime.Now;
+
+            if (tomorrow)
+            {
+                timeNow = timeNow.AddDays(1);
+            }
+            
+            return View(timeNow);
+        }
+    }
+}

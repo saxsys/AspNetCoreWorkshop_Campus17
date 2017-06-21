7145b9f084625a4eca234265d6f8d3a9021b6efd
Marcus Kohnert <marcus.dev@live.com>
2017-06-21 21:20:01 +0200

    Added partial view sample

---
+++ Slot3/Slot3/Slot3/Controllers/PartialViewController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Slot3.Controllers
+{
+    public class PartialViewController : Controller
+    {
+        public IActionResult Index() => this.View();
+    }
+}

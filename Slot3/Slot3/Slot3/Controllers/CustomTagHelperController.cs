fabab02d53e218b5245f0d7cfb13d9e240ff7d9c
Susann Schmidt <susann.schmidt@saxsys.de>
2017-06-21 17:43:23 +0200

    Add taghelper

---
+++ Slot3/Slot3/Slot3/Controllers/CustomTagHelperController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Slot3.Controllers
+{
+    public class CustomTagHelperController: Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}

cb2749dd88554a3de3c4967a9920974a726a5b84
Susann Schmidt <susann.schmidt@saxsys.de>
2017-06-21 16:11:03 +0200

    add custom taghelper controller

---
+++ Slot3/Slot3/Slot3/Controllers/CustomTagHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Slot3.Controllers
+{
+    public class CustomTagHelper
+    {
+    }
+}
